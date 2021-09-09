2b0a11f2499fce66bf0780808d1979b7eaf37b0d
Evgene Popolamov <popolamov908@gmail.com>
2021-09-09 18:16:17 +0300

    feat: use forwardref feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
@@ -6,18 +6,35 @@ import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 function App() {
-  const [posts, setsPosts] = useState([
+  const [posts, setPosts] = useState([
     { id: 1, title: 'JavaScript', body: 'Description' },
     { id: 2, title: 'JavaScript 2', body: 'Description' },
     { id: 3, title: 'JavaScript 3', body: 'Description' },
   ]);
 
+  const [title, setTitle] = useState('');
+
+  const inputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(inputRef.current.value);
+  };
+
   return (
     <div className='App'>
       <form>
-        <MyInput type='text' placeholder='Название поста' />
-        <MyInput type='text' placeholder='Описание поста' />
-        <MyButton disabled>Создать пост</MyButton>
+        {/* Управляемый компонент */}
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type='text'
+          placeholder='Название поста'
+        />
+        {/* Неуправляемый компонент */}
+        <MyInput ref={inputRef} type='text' placeholder='Описание поста' />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} />
     </div>
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import cls from './MyInput.module.css';
 
-const MyInput = (props) => {
-  return <input className={cls.myInput} {...props} />;
-};
+const MyInput = React.forwardRef((props, ref) => {
+  return <input ref={ref} className={cls.myInput} {...props} />;
+});
 
 export default MyInput;
