d0e025200073f89aa1b4907b7f57a3756a0cd507
Evgene Popolamov <popolamov908@gmail.com>
2021-09-09 22:25:15 +0300

    feat: add custom modal window

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
-import { useState, useRef } from 'react';
+import { useMemo, useState } from 'react';
+import PostFilter from './components/PostFilter';
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
-
+import MyModal from './components/UI/modal/MyModal';
 import './styles/App.css';
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'JavaScript', body: 'Description' },
-    { id: 2, title: 'JavaScript 2', body: 'Description' },
-    { id: 3, title: 'JavaScript 3', body: 'Description' },
+    { id: 1, title: 'JavaScript', body: 'Array methods' },
+    { id: 2, title: 'Node.js', body: 'Function' },
+    { id: 3, title: 'JavaScript 3', body: 'Prototype' },
   ]);
 
-  const [title, setTitle] = useState('');
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
 
-  const inputRef = useRef();
+  const sortedPost = useMemo(() => {
+    console.log('вызов функ ');
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
+    }
+    return posts;
+  }, [filter.sort, posts]);
 
-  const addNewPost = (e) => {
-    e.preventDefault();
-    console.log(title);
-    console.log(inputRef.current.value);
+  const sortedAndSerchedPosts = useMemo(() => {
+    return sortedPost.filter((post) =>
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [filter.query, sortedPost]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
   };
 
   return (
     <div className='App'>
-      <form>
-        {/* Управляемый компонент */}
-        <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          type='text'
-          placeholder='Название поста'
-        />
-        {/* Неуправляемый компонент */}
-        <MyInput ref={inputRef} type='text' placeholder='Описание поста' />
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} />
+      <MyButton style={{ marginTop: '20px' }} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
+      <hr style={{ margin: '15px 0' }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList posts={sortedAndSerchedPosts} remove={removePost} />
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import PostItem from './PostItem';
 
-function PostList({ posts }) {
+function PostList({ posts, remove }) {
+  if (!posts.length) {
+    return <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>;
+  }
+
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Список постов</h1>
-      {posts.map((post) => {
-        return <PostItem post={post} key={post.id} />;
+      {posts.map((post, i) => {
+        return (
+          <PostItem remove={remove} number={i + 1} post={post} key={post.id} />
+        );
       })}
     </div>
   );
